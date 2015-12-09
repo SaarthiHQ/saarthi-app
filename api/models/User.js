25c2501767996c85e3dec6c484869f8e95609ef6
raisedadead <killerinstinct4u@gmail.com>
2015-12-09 12:11:23 +0000

    Added and configured adapter for MongoDB

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+module.exports = {
+    attributes: {
+        role: {
+            type: 'string',
+            defaultsTo: 'Consumer' // Can be Consumer or Conductor
+        },
+        name: {
+            type: 'string',
+            required: true
+        },
+        email: {
+            type: 'email',
+            required: true,
+            unique: true
+        },
+        contact: {
+            type: 'string',
+            required: true,
+            unique: true
+        }//,
+        //lastTrips: {
+        //    type: 'array'
+        //}
+    }
+};
