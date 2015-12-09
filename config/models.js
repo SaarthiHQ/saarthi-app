0727cad83a77c142fc755a48fc4467ee2668638e
raisedadead <killerinstinct4u@gmail.com>
2015-12-09 13:11:56 +0000

    Fix config

--- config/models.js
+++ config/models.js
@@ -27,7 +27,7 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  migrate: 'safe'
+  migrate: 'safe',
 
   /**************************************
   *                                     *
@@ -36,6 +36,6 @@ module.exports.models = {
   **************************************/
 
   // Auto Primary Key
-  autoPK: true,
+  autoPK: true
 
 };
