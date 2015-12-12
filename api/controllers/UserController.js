31b97bc50226f636e56be2e796c3b589c8afb163
raisedadead <killerinstinct4u@gmail.com>
2015-12-12 19:57:52 +0000

    Upate User API

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,26 @@
  */
 
 module.exports = {
-	
+  findAll: function (req, res) {
+    User.find().exec(function (err, users) {
+      if (err) {
+        res.send(400);
+      } else {
+        res.send(users);
+      }
+    });
+  },
+
+  findByEmail: function(req, res) {
+    var email = req.param('email');
+    console.log("Email : " + email);
+    User.findByEmail(email).exec(function (err, users) {
+      if (err) {
+        res.send(400);
+      } else {
+        res.send(users);
+      }
+    });
+  }
 };
 
