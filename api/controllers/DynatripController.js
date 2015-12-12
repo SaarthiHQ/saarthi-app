b0b96aaeca8db9028e53994cebbe67077bf4a92c
Mrugesh Mohapatra <me@raisedadead.com>
2015-12-12 12:36:42 +0530

    Add Dynamic Trip

---
+++ api/controllers/DynatripController.js
@@ -0,0 +1,11 @@
+/**
+ * DynatripController
+ *
+ * @description :: Server-side logic for managing dynatrips
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
